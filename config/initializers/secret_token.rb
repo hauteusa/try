69d5025695cf9ac25b7bf85f741fa79bc7e6ba8f
hauteusa <hauteusa@gmail.com>
2011-11-26 22:24:47 +0530

    first commit todos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'ba6c87d66e7825882e1c13873f04fbd095d3eeeb682868f54244209b3e3ec1ee4527f8f9722fe0bf55dc31b19f82c8caf32cb62f32ec61699a1d905c4cf51a86'
